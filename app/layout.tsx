18c3ec37008044f7d9c7f623d35e3fa8607464d8
Joshua Forster <joshuaforster95@gmail.com>
2026-09-04 17:50:23 +0100

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,9 @@ export const metadata: Metadata = {
   },
   description:
     "Integrity Fitness Education offers professional personal trainer courses and fitness qualifications in Norwich, Norfolk. Start your fitness career today.",
+  icons: {
+    icon: 'images/ife.ico',
+  },
   openGraph: {
     siteName: "Integrity Fitness Education",
     locale: "en_GB",
